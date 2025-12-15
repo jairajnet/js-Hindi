adcfaea85e5456714ca2397cc8a99be83e26cb55
Jairaj Singh <jairaj.net@gmail.com>
2025-12-15 13:21:42 +0000

    conversion

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let age="33q"
+
+let newnumber=Number(age);
+console.log(typeof age);
+console.log(typeof newnumber);
+console.log(newnumber);
+
+// 33=>33
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
