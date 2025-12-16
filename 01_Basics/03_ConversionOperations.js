586c835a0313db30393b2ca8c881aab80ac6f5d2
Jairaj Singh <jairaj.net@gmail.com>
2025-12-16 13:28:37 +0000

    operations

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -3,12 +3,29 @@
 let age="33q"
 
 let newnumber=Number(age);
-console.log(typeof age);
-console.log(typeof newnumber);
-console.log(newnumber);
+// console.log(typeof age);
+// console.log(typeof newnumber);
+// console.log(newnumber);
 
 // 33=>33
 // "33" => 33
 // "33abc" => NaN
 // true => 1
 // false => 0
+
+/* Operations ------------------------*/
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+// console.log(+true);
+
+let gameCounter=100;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
+
+
