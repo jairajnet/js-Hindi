1690d226ec60164b2b952e61ee9cb37e9cf1dc17
Jairaj Singh <jairaj.net@gmail.com>
2025-12-17 13:33:29 +0000

    datatype-comparisions

---
+++ 01_Basics/dtatypes-summary.js
@@ -0,0 +1,12 @@
+// Primitive
+// 7 : number,string,null,boolean,undefined, symbol, nigInt
+
+// Reference type 
+// Array, Objects, functions
+
+
+const id=Symbol('122')
+let anotherID=Symbol['123']
+let bigNumber=126783483743897123213123n
+//console.log(typeof bigNumber);
+console.log(typeof id);
