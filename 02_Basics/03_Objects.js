4f7dc5db23749a2429f550e5a76ad809ac3381cd
Jairaj Singh <jairaj.net@gmail.com>
2025-12-25 12:47:34 +0000

    objects

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,14 @@
+//singleton--> when created by a constructor
+//Object.create
+
+//object literals
+
+const JsUser={
+    name:"Jairaj",
+    age:40,
+    location:"Udaipur",
+    email:"jai@gmail.com",
+    isloggedIN: false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
