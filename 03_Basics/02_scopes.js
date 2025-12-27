458530ec6a74e21cc6c69dc56b055134969c6c8e
Jairaj Singh <jairaj.net@gmail.com>
2025-12-27 15:11:43 +0000

    scope1

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,12 @@
+// let a =10
+// const b=20
+// var c=30
+if (true){
+let a =10
+const b=20
+var c=30
+}
+
+//console.log(a); //defined in the if condition then cant be used outside that scope
+//console.log(b); //defined in the if condition then cant be used outside that scope
+console.log(c); // it shows that var can be used outside the scope also.
