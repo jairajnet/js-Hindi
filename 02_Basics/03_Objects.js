58c39fdbf49e51989e0abb9a7b9d5073eb040e6b
Jairaj Singh <jairaj.net@gmail.com>
2025-12-27 10:26:05 +0000

    objectsPart1

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -2,9 +2,11 @@
 //Object.create
 
 //object literals
-
+const mySym=Symbol("Key1")
 const JsUser={
     name:"Jairaj",
+    "full name":"Jairaj Singh",
+    [mySym]:"Keyword1",
     age:40,
     location:"Udaipur",
     email:"jai@gmail.com",
@@ -12,3 +14,28 @@ const JsUser={
     lastLoginDays:["Monday","Saturday"]
 }
 
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email="New Value" // to assign new value to the property of the object
+
+//Object.freeze(JsUser); // to freeze the object
+JsUser.email="Newest value";
+
+//console.log(JsUser);
+
+//function in object
+JsUser.greeting=function(){
+    console.log("Hello JS User");
+}
+
+
+
+JsUser.greetingTwo=function(){
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
