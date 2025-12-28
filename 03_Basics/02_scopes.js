a648c010c9e329b8204ca1843d0b97da110290c2
Jairaj Singh <jairaj.net@gmail.com>
2025-12-28 08:24:59 +0000

    scope

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -10,3 +10,17 @@ var c=30
 //console.log(a); //defined in the if condition then cant be used outside that scope
 //console.log(b); //defined in the if condition then cant be used outside that scope
 console.log(c); // it shows that var can be used outside the scope also.
+
+//++++++++++++ Mini Hoisting++++++++++++++++++++++++
+
+console.log(addOne(5))
+function addOne(num){
+    return num+1
+}
+
+
+//console.log( AddTwo(8)) // cannot access this addtwo because it is defined below this
+
+const AddTwo=function(num){
+    return num+2
+}
