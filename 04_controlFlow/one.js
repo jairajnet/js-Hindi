f0d82a95b7231987c298a4aa341bfc829863080f
Jairaj Singh <jairaj.net@gmail.com>
2025-12-28 14:25:04 +0000

    controlflow1

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,8 @@
+// if (2=="2"){
+if (2==="2"){
+    console.log("executed");
+    }
+    else{
+        console.log("not executed");
+    }
+
---
+++ 04_controlFlow/truthy.js
@@ -0,0 +1,17 @@
+// let username="jai@ajai" // will return true
+let username="" // will return false
+
+if (username){
+    console.log("have email");
+    }
+    else
+    {
+        console.log("Dont have email");
+        
+    }
+
+    // falsy value-->
+    //false, 0, -0, BigInt 0m, "", null, undefined, NaN
+
+    //truthy-->
+    //"0", 'false', " ",[], {}, function(){}
